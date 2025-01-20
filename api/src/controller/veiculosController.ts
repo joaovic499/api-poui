772b88426778fd662f1f1b865fde4426a66f55bf
João Victor Silva <joaovictor.silva@itmil.com.br>
2025-01-20 15:04:37 -0300

    commit criado get veiculos

--- api/src/controller/veiculosController.ts
+++ api/src/controller/veiculosController.ts
@@ -8,13 +8,14 @@ export const criarVeiculo = async (req: Request, res: Response, next: NextFuncti
     const {chassi, modelo, cor, estado, armazem, procedencia, userId, marca} = req.body
 
     try {
-        if (!chassi || !modelo || !cor || !estado || !armazem || !procedencia || !marca ) {
+        if (!chassi || !modelo || !cor || !estado || !armazem || !procedencia || !marca || !userId ) {
         res.status(400).json({message: "Erro ao cadastrar veicluos"})
         return;
         }
 
         const newVeiculos = await prisma.veiculo.create({
             data: {
+                userId,
                 chassi,
                 modelo,
                 cor,
@@ -22,7 +23,6 @@ export const criarVeiculo = async (req: Request, res: Response, next: NextFuncti
                 estado,
                 armazem,
                 procedencia,
-                userId
         }
     });
 
@@ -36,4 +36,35 @@ export const criarVeiculo = async (req: Request, res: Response, next: NextFuncti
         console.log("teste")
     }
 
+}
+
+export const getVeiculos = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const {idUser} = req.params;
+
+    try {
+        if (!idUser) {
+            res.status(400).json({message: "Id do usuário obrigatório"});
+            return
+        }
+
+        const veiculos = await prisma.veiculo.findMany({
+            where: {
+                userId: idUser
+            },
+            select: {
+                chassi: true,
+                modelo: true,
+                marca: true,
+                estado: true,
+                armazem: true,
+                procedencia: true,
+            }
+        });
+
+        res.json(veiculos)
+    } catch (error) {
+        console.log("Erro ao buscar veiculos", error)
+        res.status(500).send("Erro ao buscar veiculos")
+        next();
+    }
 }
--- api/src/routes/authRoutes.ts
+++ api/src/routes/authRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { register, login } from '../controller/authController';
-import { criarVeiculo } from '../controller/veiculosController';
+import { criarVeiculo, getVeiculos } from '../controller/veiculosController';
 
 const router = Router();
 router.post('/register', register)
 router.post('/login', login)
-
 router.post('/create/chassi', criarVeiculo)
+
+router.get('/get/veiculos/:idUser', getVeiculos)
 
 export default router;
